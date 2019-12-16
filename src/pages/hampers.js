37df8b3c55c7e1c37ed61d82b4afbee77b78a67f
David Havelin <david@havel.in>
2019-12-16 00:03:17 +0000

    Add hamper providers

--- src/pages/hampers.js
+++ src/pages/hampers.js
@@ -9,29 +9,18 @@ const HampersPage = () => {
 
   const data = useStaticQuery(graphql`
     query {
-      neat: file(relativePath: { eq: "images/hampers/wineonline.jpg" }) {
+      wineonline: file(relativePath: { eq: "images/hampers/wineonline.jpg" }) {
         ...hamperImage
       }
 
-      ginandtonic: file(relativePath: { eq: "images/hampers/wineonline.jpg" }) {
+      headline: file(relativePath: { eq: "images/hampers/57hamper.jpg" }) {
         ...hamperImage
       }
 
-      ginbuck: file(relativePath: { eq: "images/hampers/wineonline.jpg" }) {
+      baggotstreet: file(relativePath: { eq: "images/hampers/baggotstreet.jpg" }) {
         ...hamperImage
       }
 
-      martini: file(relativePath: { eq: "images/hampers/wineonline.jpg" }) {
-        ...hamperImage
-      }
-
-      hankypanky: file(relativePath: { eq: "images/hampers/wineonline.jpg" }) {
-        ...hamperImage
-      }
-
-      hotpunch: file(relativePath: { eq: "images/hampers/wineonline.jpg" }) {
-        ...hamperImage
-      }
     }
   `)
 
@@ -46,22 +35,33 @@ const HampersPage = () => {
         <h2><a href="https://wineonline.ie/collections/chinnery-spirits/products/the-chinnery-gin-hamper">Wine Online</a></h2>
 
         <p>Each hamper contains a 70cl bottle of Chinnery Dublin Dry Gin, two bottles of premium Poacher’s “Wild” Tonic, new Lismore Dark Chocolate Apple Sticks and a luxury candle from Irish brand Purcell &amp; Woodcock.</p>
-        <p></p>
         <a href="https://wineonline.ie/collections/chinnery-spirits/products/the-chinnery-gin-hamper">
           <figure style={{textAlign: "center"}}>
-            <Img fixed={data.ginandtonic.childImageSharp.fixed} />
+            <Img fixed={data.wineonline.childImageSharp.fixed} />
           </figure>
         </a>
 
-        <h2><a href="https://wineonline.ie/collections/chinnery-spirits/products/the-chinnery-gin-hamper">Wine Online</a></h2>
+        <h2><a href="https://57theheadline.com/product/gin-hampers/">57 The Headline</a></h2>
 
-        <p>Each hamper contains a 70cl bottle of Chinnery Dublin Dry Gin, two bottles of premium Poacher’s “Wild” Tonic, new Lismore Dark Chocolate Apple Sticks and a luxury candle from Irish brand Purcell &amp; Woodcock.</p>
-        <p></p>
-        <a href="https://wineonline.ie/collections/chinnery-spirits/products/the-chinnery-gin-hamper">
+        <p>Each hamper contains one bottle of Chinnery Gin, 2 Chinnery Gin glasses, 4 bottles of premium tonic and 3 types of garnish. All packed into a cardboard, wrapped hamper.</p>
+        <a href="https://57theheadline.com/product/gin-hampers/">
           <figure style={{textAlign: "center"}}>
-            <Img fixed={data.ginandtonic.childImageSharp.fixed} />
+            <Img fixed={data.headline.childImageSharp.fixed} />
           </figure>
         </a>
+
+
+        <h2><a href="http://baggotstreetwines.com/">Baggot Street Wines</a></h2>
+
+        <p>Each hamper contains one bottle of Chinnery Gin, 3 bottles of Poachers Wild tonic and a Gin Fusion Kit.</p>
+        <a href="https://57theheadline.com/product/gin-hampers/">
+          <figure style={{textAlign: "center"}}>
+            <Img fixed={data.baggotstreet.childImageSharp.fixed} />
+          </figure>
+        </a>
+
+        <h2>Also...</h2>
+        <p>These fine off licences can make up a Chinnery Gin hamper for you on request: <a href="https://www.blackrockcellar.com/">Blackrock Cellar</a>, <a href="https://mitchellandson.com/">Mitchell &amp; Son</a>, <a href="https://mchughs.ie/">McHugh's</a> and <a href="https://jamesfox.ie/">James Fox</a>.</p>
 
       </article>
     </Layout>
@@ -73,7 +73,7 @@ export default HampersPage
 export const hamperImage = graphql`
   fragment hamperImage on File {
     childImageSharp {
-      fixed(width: 300) {
+      fixed(width: 500) {
         ...GatsbyImageSharpFixed
       }
     }
